25849faa6e47c3e303543c4b6ef3590e47abf4f2
lw <123@test.com>
2019-09-24 23:08:58 +0800

    edit

--- js/global.js
+++ js/global.js
@@ -9,16 +9,25 @@ $("body em").each(function(){
     });
     var text = ''
     for(var i = 0; i < newValue.length; i++) {
-        text += ('<span class="letter">' + newValue[i] + '</span>')
+        var letter = newValue[i]
+        if(letter === ' '){
+            letter = '空格'
+        }
+        text += ('<span class="letter">' + letter + '</span>')
     }
-    var _html = '<input class="input" readonly="true" type="text" /><div class="word" style="display:inline;">[(' + text + ')<p class="clear-input" style="display:inline;">clear</p><p class="answer" style="display:inline;">answer</p>]</div>'
+    var _html = '<span class="input" ><span class="empty">( </span><span class="input-value"></span><span class="empty"> )</span></span><div class="word" style="display:inline;">[' + text + '<p class="clear-input" style="display:inline;">clear</p><p class="answer" style="display:inline;">answer</p>]</div>'
     $(this).html(_html)
 })
 
 $(".word .letter").on("click",function(){
-    var value = $(this).parent().siblings("input").val()
-    value = value + $(this).text()
-    $(this).parent().siblings("input").val(value)
+    //$(this).parents(".target").find(".input .empty").addClass("hide")
+    var value = $(this).parents(".target").find(".input-value").text()
+    var letter = $(this).text()
+    if(letter === '空格'){
+        letter = ' '
+    }
+    value = value + letter
+    $(this).parents(".target").find(".input-value").text(value)
     var word = $(this).parents('.target').attr('word')
     if(value === word){
         $(this).parents('.target').addClass("right")
@@ -27,12 +36,13 @@ $(".word .letter").on("click",function(){
     }
 })
 $(".answer").on("click",function(){
+    //$(this).parents(".target").find(".input .empty").addClass("hide")
     var word = $(this).parents('.target').attr('word')
-    $(this).parent().siblings("input").val(word)
+    $(this).parents(".target").find(".input-value").text(word)
     $(this).parents('.target').addClass("right")
 })
-$(".clear").on("click",function(){
-    $(this).parents(".target").find("input").val('')
+$(".clear-input").on("click",function(){
+    $(this).parents(".target").find(".input-value").text('')
     $(this).parents('.target').removeClass("right")
 })
 
