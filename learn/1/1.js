a04b0e288630180f9755bb4955ba83a6493d33c6
lwhappy <liaowei2006_2006@126.com>
2019-10-31 22:30:26 +0800

    add

---
+++ learn/1/1.js
@@ -0,0 +1,48 @@
+var data = {
+   title:'第一集',
+   part:
+    [
+      {
+        time:5,
+        rows: [
+          {
+            word:{english:'good',sound:'[ɡʊd]',chinese:'好',matchWord:null},
+            sentence:'he is a good man'
+          },
+          {
+            word:{english:'bad man',sound:'',chinese:'坏',matchWord:'bad,man'},
+            sentence:'he is a bad man'
+          },
+          {
+            word:{english:'smart',sound:'',chinese:'聪明',matchWord:null},
+            sentence:'he is a smart man'
+          },
+          {
+            word:{english:'beautiful',sound:'',chinese:'好',matchWord:null},
+            sentence:'he is a beautiful man'
+          }
+        ]
+      },
+      {
+        time:10,
+        rows: [
+          {
+            word:{english:'good',sound:'[ɡʊd]',chinese:'好',matchWord:null},
+            sentence:'he is a good boy'
+          },
+          {
+            word:{english:'bad boy',sound:'',chinese:'坏',matchWord:'bad,boy'},
+            sentence:'he is a bad boy'
+          },
+          {
+            word:{english:'smart',sound:'',chinese:'聪明',matchWord:null},
+            sentence:'he is a smart boy'
+          },
+          {
+            word:{english:'beautiful',sound:'',chinese:'好',matchWord:null},
+            sentence:'he is a beautiful boy'
+          }
+        ]
+      }
+  ]
+}
